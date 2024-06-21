df04136f8a80329b78bde91cb194e75792020f8b
Mark Sergienko <ffab00@gmail.com>
2024-06-21 10:58:29 +0100

    fix types

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,13 +9,17 @@ export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
   return (
+    // @ts-ignore
     <Html lang={currentLocale}>
+      {/* @ts-ignore */}
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
       </Head>
       <body>
+        {/* @ts-ignore */}
         <Main />
+        {/* @ts-ignore */}
         <NextScript />
       </body>
     </Html>
