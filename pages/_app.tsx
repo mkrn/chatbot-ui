32051c440113d71a8a8cbb462611f3ed667204df
Mark Sergienko <ffab00@gmail.com>
2024-06-21 10:52:49 +0100

    ignore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,16 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+import type { NextComponentType, NextPageContext } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function App({ Component, pageProps }: AppProps<{}>) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
+      {/* @ts-ignore */}
       <Toaster />
+      {/* @ts-ignore */}
       <Component {...pageProps} />
     </div>
   );
