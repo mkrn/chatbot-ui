c5142acfdf5c0e99c0b04cdf31fbabca326d8f92
Mark Sergienko <ffab00@gmail.com>
2024-07-09 11:35:02 +0100

    test bot

---
+++ pages/api/botwebhook.js
@@ -0,0 +1,18 @@
+import { bot, secretToken } from '@/utils/bot';
+import { webhookCallback } from 'grammy';
+
+export const config = { runtime: 'edge' };
+
+// Default grammY handler for incoming updates via webhooks
+export default async function handler(req) {
+  if (req.method === 'POST') {
+    const callback = webhookCallback(bot, 'std/http', {
+      timeoutMilliseconds: 24_000,
+      secretToken,
+    });
+
+    return await callback(req);
+  } else {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+}
---
+++ utils/bot.js
@@ -0,0 +1,19 @@
+import { Bot } from 'grammy';
+
+export const {
+  // Telegram bot token from t.me/BotFather
+  TELEGRAM_BOT_TOKEN: token,
+
+  // Secret token to validate incoming updates
+  TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
+} = process.env;
+
+// Default grammY bot instance
+export const bot = new Bot(token);
+
+// Reply with link to mini app when user sends a message
+bot.on('message:text', (ctx) => {
+  //   const keyboard = new InlineKeyboard().game('Start Mini App');
+  //   ctx.replyWithGame('my_mini_app', { reply_markup: keyboard });
+  ctx.reply('Hi!');
+});
