a7a51ef4aac9f02121ae27d4d0cdf111439e93da
Mark Sergienko <ffab00@gmail.com>
2024-07-09 12:44:09 +0100

    botnew

--- pages/api/botwebhook.js
+++ pages/api/botwebhook.js
@@ -4,21 +4,24 @@ import { webhookCallback } from 'grammy';
 export const config = { runtime: 'edge' };
 
 // Default grammY handler for incoming updates via webhooks
-export default async function handler(req) {
-  console.log('webhook call', req, secretToken);
 
-  // Log all searchParams
-  const url = new URL(req.url);
-  console.log('Search Params:', Object.fromEntries(url.searchParams));
+export default webhookCallback(bot, 'std/http');
 
-  if (req.method === 'POST') {
-    const callback = webhookCallback(bot, 'std/http', {
-      timeoutMilliseconds: 24_000,
-      secretToken,
-    });
+// export default async function handler(req) {
+//   console.log('webhook call', req, secretToken);
 
-    return await callback(req);
-  } else {
-    return new Response('Method Not Allowed', { status: 405 });
-  }
-}
+//   // Log all searchParams
+//   const url = new URL(req.url);
+//   console.log('Search Params:', Object.fromEntries(url.searchParams));
+
+//   if (req.method === 'POST') {
+//     const callback = webhookCallback(bot, 'std/http', {
+//       timeoutMilliseconds: 24_000,
+//       secretToken,
+//     });
+
+//     return await callback(req);
+//   } else {
+//     return new Response('Method Not Allowed', { status: 405 });
+//   }
+// }
