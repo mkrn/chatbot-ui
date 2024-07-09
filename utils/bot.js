d115c813c7b5fb08ed2e4294caa12b06f82e210f
Mark Sergienko <ffab00@gmail.com>
2024-07-09 12:50:42 +0100

    botnew2

--- utils/bot.js
+++ utils/bot.js
@@ -12,10 +12,28 @@ export const {
 export const bot = new Bot(token);
 
 // Reply with link to mini app when user sends a message
-bot.on('message:text', (ctx) => {
-  console.log('message text', ctx);
+bot.on('message:text', async (ctx) => {
+  console.log('message text', ctx.message.text);
+
+  const res = await fetch(`${OPENAI_API_HOST}/chat/completions`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key ? key : process.env.OPENAI_API_KEY}`,
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      model: 'meta-llama-3-70b-instruct',
+      messages: [ctx.message.text],
+      // max_tokens: 1000,
+      temperature: 1,
+      stream: false,
+    }),
+  });
+
+  const data = await res.json();
+  console.log('data', data);
 
   //   const keyboard = new InlineKeyboard().game('Start Mini App');
   //   ctx.replyWithGame('my_mini_app', { reply_markup: keyboard });
-  ctx.reply('Hi!');
+  ctx.reply(data.choices[0].message.content);
 });
