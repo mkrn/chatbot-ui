2a467abb93b66eb06af746f8973c1c97e66c0cfe
Mark Sergienko <ffab00@gmail.com>
2024-07-09 11:39:58 +0100

    debug

--- pages/api/botwebhook.js
+++ pages/api/botwebhook.js
@@ -6,6 +6,7 @@ export const config = { runtime: 'edge' };
 // Default grammY handler for incoming updates via webhooks
 export default async function handler(req) {
   if (req.method === 'POST') {
+    console.log('webhook call', req);
     const callback = webhookCallback(bot, 'std/http', {
       timeoutMilliseconds: 24_000,
       secretToken,
